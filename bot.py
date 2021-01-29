17c1320416ac71957ede316bcc746f659a8723d9
tre3x <gangulyofficialshreyan@gmail.com>
2021-01-30 01:15:32 +0530

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+from core import booze
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$drink'):
+        drink = message.content[6:]
+        drinks = booze(drink, 0, 0)
+        if(len(drinks) == 0): await message.channel.send("Beep.Boop. No drinks found! 🥺")
+        else:
+            for drink in drinks: 
+                await message.channel.send(drink[0] + " --- " + drink[1])
+            await message.channel.send("Happy Drinking! 🍻")
+
+    if message.content.startswith('$rngdrink'):
+        text = message.content[9:].split()
+        low = text[0]
+        high = text[1]
+        drink = " ".join(text[2:])
+        drinks = booze(drink, low, high)
+        if(len(drinks) == 0): await message.channel.send("Beep.Boop. No drinks found! 🥺")
+        else:
+            for drink in drinks: 
+                await message.channel.send(drink[0] + " --- " + drink[1])
+            await message.channel.send("Happy Drinking! 🍻")
+
+client.run('ODA0NTkwMDczNjAwNjcxNzU0.YBOi5g.3RV2cQVcSRCo8OXMmPuFhy_VPss')
---
+++ core.py
@@ -0,0 +1,28 @@
+import bs4
+import requests
+
+def booze(tag, low, high):
+    if(low == 0 and high == 0): link = 'https://www.spencers.in/catalogsearch/result/index/?cat=10664&q=' + tag
+    else: link = 'https://www.spencers.in/catalogsearch/result/index/?cat=10664&price=' + low + '-' + high + '&q=' + tag
+
+    res = requests.get(link)
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    output = []
+    
+    for ol in soup.find_all("ol", {"class":"products list items product-items"}):
+        for li in ol.find_all(("li", {"class" : "item product product-item col-md-3 col-sm-3 col-xs-6"}) or ("li", {"class" : "item product product-item col-md-3 col-sm-3 col-xs-6 last_product"})):
+            pricetag = []
+            for a in li.find_all("a", {"class" : "product-item-link"}):
+                pricetag.append(a.text.lstrip().rstrip())
+            for span in li.find_all("span", {"class" : "price"}):
+                pricetag.append(span.text)
+            output.append(pricetag)
+    if(len(output) > 0): output.pop(-1)
+    return output
+'''
+tag = input("Enter your wonder : ")
+output = booze(tag)
+if(len(output) > 0): output.pop(-1)
+for i in output:
+    print(i)
+'''
