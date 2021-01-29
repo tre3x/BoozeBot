394ce6ad336d1df844e0ee3b76e7fe5a877d0d32
tre3x <gangulyofficialshreyan@gmail.com>
2021-01-30 02:16:47 +0530

    Committed

--- bot.py
+++ bot.py
@@ -31,4 +31,7 @@ async def on_message(message):
                 await message.channel.send("```CSS\n"+drink[0] + " --> " + drink[1]+"\n```")
             await message.channel.send("Happy Drinking! 🍻")
 
+    if message.content.startswith('$help'):
+        await message.channel.send("```\n For getting Price of a specific catagory or a specific drink \n write   $drink <Drink Name> \n\n For getting Price of a specific catagory or a specific drink within a specific price range \n write  $drink <Lower Price Limit> <Higher Price Limit> <Drink Name> \n```")
+
 client.run('ODA0NTkwMDczNjAwNjcxNzU0.YBOi5g.3RV2cQVcSRCo8OXMmPuFhy_VPss')
